5adbbe8ff98f0e67e51d38523ce301f4a425f181
Maximilian Mundt <maxi-mundt@t-online.de>
2022-06-13 19:17:33 +0200

    Final update

--- Übungsblatt 08/handout.py
+++ Übungsblatt 08/handout.py
@@ -5,35 +5,31 @@ from torchvision import datasets
 from torchvision import transforms
 from datetime import datetime
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class CIFAR10Classifier(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.convs = nn.Sequential(
-            *self._create_conv_block(3, 32),
-            *self._create_conv_block(32, 32),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            *self._create_conv_block(32, 64),
+            *self._create_conv_block(3, 64),
             *self._create_conv_block(64, 64),
             nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.AdaptiveAvgPool2d((7, 7)),
+            *self._create_conv_block(64, 128),
+            *self._create_conv_block(128, 128),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.AdaptiveAvgPool2d((6, 6)),
         )
         self.fully_connected = nn.Sequential(
-            nn.Linear(64 * 7 * 7, 512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 10),
+            nn.Linear(128 * 6 * 6, 256), nn.ReLU(inplace=True), nn.Dropout(0.2), nn.Linear(256, 10),
         )
 
     def _create_conv_block(self, in_channels, out_channels):
         return [
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
         ]
 
     def forward(self, x):
@@ -47,6 +43,13 @@ class CIFAR10Classifier(nn.Module):
         torchsummary.summary(self, (3, 32, 32))
 
 
+def to_device(data, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+
+    return data.to(device, non_blocking=True)
+
+
 def train(model, dataloader, epochs=10, save=False):
     model.train()
     optimizer = torch.optim.Adam(model.parameters())
@@ -54,18 +57,23 @@ def train(model, dataloader, epochs=10, save=False):
     loss_history = []
 
     for epoch in range(1, epochs + 1):
+        print(f"Beginning epoch {epoch} / {epochs}: ", end="")
         running_loss = []
-        print(f"Beginning epoch {epoch} / {epochs}")
 
-        for _, (images, labels) in enumerate(dataloader, 1):
+        for _, data in enumerate(dataloader, 1):
+            images, labels = to_device(data, DEVICE)
+
             optimizer.zero_grad()
+
             outputs = model(images)
             loss = cross_entropy(outputs, labels)
             loss.backward()
-            running_loss.append(loss.item())
             optimizer.step()
 
+            running_loss.append(loss.item())
+
         epoch_loss = torch.Tensor(running_loss).mean().item()
+        print(f"loss = {epoch_loss:.4f}")
         loss_history.append(epoch_loss)
 
     if save:
@@ -81,7 +89,9 @@ def evaluate(model, dataloader):
     correct = 0
     total = 0
 
-    for images, labels in dataloader:
+    for data in dataloader:
+        images, labels = to_device(data, DEVICE)
+
         outputs = model(images)
         predicted_labels = torch.argmax(outputs.data, 1)
 
@@ -104,5 +114,5 @@ def create_augmented_dataset(train):
 
 
 if __name__ == "__main__":
-    model = CIFAR10Classifier()
+    model = CIFAR10Classifier().to(DEVICE)
     model.summary()
--- Übungsblatt 08/visualize.py
+++ Übungsblatt 08/visualize.py
@@ -1,4 +1,5 @@
 from typing import Union, List
+from matplotlib import image
 import torch as th
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,7 +16,13 @@ def show_loss_curve(loss_curve: List) -> None:
 
 
 @th.no_grad()
-def show_image_grid(images: th.Tensor, labels: th.Tensor, predictions: th.Tensor = None) -> None:
+def show_image_grid(
+    images: th.Tensor, labels: th.Tensor, predictions: th.Tensor = th.Tensor([])
+) -> None:
+    images = images.cpu()
+    labels = labels.cpu()
+    if len(predictions) != 0:
+        predictions = predictions.cpu()
     classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
     size = math.ceil(math.sqrt(len(images)))
     _, axs = plt.subplots(size, size, figsize=(size * 2, size * 2))
@@ -24,13 +31,14 @@ def show_image_grid(images: th.Tensor, labels: th.Tensor, predictions: th.Tensor
         for y in x:
             y.set_axis_off()
 
-    for idx, (img, label, pred) in enumerate(zip(images, labels, predictions)):
+    for idx, (img, label) in enumerate(zip(images, labels)):
         pos = idx // size, idx % size
         axs[pos].imshow(img.squeeze().permute(1, 2, 0).numpy())
 
-        if predictions is None:
+        if len(predictions) == 0:
             axs[pos].set_title(f"Actual: {classes[label]}")
         else:
+            pred = predictions[idx]
             axs[pos].set_title(f"Actual: {classes[label]} \n Prediction: {classes[pred]}")
 
     plt.tight_layout()
